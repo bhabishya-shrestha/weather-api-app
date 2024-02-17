514fad074bc31ad4dabfb0f5d8455f3bdb0b4a84
Bhabishya Shrestha <bhabisyas@gmail.com>
2024-02-17 14:31:59 -0600

    add search button

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -1,5 +1,10 @@
+import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class WeatherAppGui extends JFrame {
@@ -26,5 +31,23 @@ public class WeatherAppGui extends JFrame {
         searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         add(searchTextField);
+
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+    }
+
+    private ImageIcon loadImage(String resourcePath) {
+        try {
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            return new ImageIcon(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Could not find resource");
+        return null;
     }
 }
